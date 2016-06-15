3d20f57a1e2b431e4124e364b18e7de03feb4db1
Rubens Souza <r_henrique@yahoo.com>
2016-06-15 00:54:00 -0300

    added custom rake tasks to config/deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -74,3 +74,13 @@ namespace :deploy do
   end
 
 end
+
+namespace :rake do
+  desc "Invoke rake task"
+  task :invoke do
+    run "cd #{deploy_to}/current"
+    run "bundle exec rake #{ENV['task']} RAILS_ENV=#{rails_env}"
+  end
+end
+
+
